5221caefce92ba36d714aba833e637dbd0e29398
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2024-02-15 20:26:26 +0530

    added bg in particular activity page and fixed about text in the same page

--- components/ActivityContent.tsx
+++ components/ActivityContent.tsx
@@ -4,29 +4,33 @@ export const ActivityContent = () => {
   return (
     <>
       <div className={styles.aboutMain}>
-        <img src="/activity.png" className={styles.img} alt="" />
-        <div className={styles.aboutDiv}>
-          <h1>ABOUT</h1>
-          <p>
-            Potent felis facilisi feugiat volutpat amet. Elementum sed quisque
-            eget vulputate et neque, cursus tellus. Adipiscing cras pellentesque
-            velit commodo. Et dictumst a cras nisl facilisis cursus dolor.
-            Ornare morbi lobortis tristique diam. Adipiscing sed at ut sit et,
-            consequat. Ornare diam mattis eu nunc ornare erat. Vehicula
-            ultricies orci, nunc vel. Vestibulum lacus, cursus tellus
-            consectetur nisl lorem ullamcorper non. Arcu ipsum, congue tortor
-            non eget. Fermentum, platea sit mi vulputate et nisl. In ornare
-            habitasse tempus, tempor. Purus in id quisque viverra. Potent felis
-            facilisi feugiat volutpat amet. Elementum sed quisque eget vulputate
-            et neque, cursus tellus. Adipiscing cras pellentesque velit commodo.
-            Et dictumst a cras nisl facilisis cursus dolor. Ornare morbi
-            lobortis tristique diam. Adipiscing sed at ut sit et, consequat.
-            Ornare diam mattis eu nunc ornare erat. Vehicula ultricies orci,
-            nunc vel. Vestibulum lacus, cursus tellus consectetur nisl lorem.
-          </p>
-          <input className={styles.expand_btn} type="checkbox" />
-          <div className={styles.buttonDiv}>
-            <button className={styles.button}>Register Now</button>
+        <h1 className={styles.mobileAbout}>ABOUT</h1>
+        <div className={styles.imgContentWrapper}>
+          <img src="/activity.png" className={styles.img} alt="" />
+          <div className={styles.aboutDiv}>
+            <h1>ABOUT</h1>
+            <p>
+              Potent felis facilisi feugiat volutpat amet. Elementum sed quisque
+              eget vulputate et neque, cursus tellus. Adipiscing cras
+              pellentesque velit commodo. Et dictumst a cras nisl facilisis
+              cursus dolor. Ornare morbi lobortis tristique diam. Adipiscing sed
+              at ut sit et, consequat. Ornare diam mattis eu nunc ornare erat.
+              Vehicula ultricies orci, nunc vel. Vestibulum lacus, cursus tellus
+              consectetur nisl lorem ullamcorper non. Arcu ipsum, congue tortor
+              non eget. Fermentum, platea sit mi vulputate et nisl. In ornare
+              habitasse tempus, tempor. Purus in id quisque viverra. Potent
+              felis facilisi feugiat volutpat amet. Elementum sed quisque eget
+              vulputate et neque, cursus tellus. Adipiscing cras pellentesque
+              velit commodo. Et dictumst a cras nisl facilisis cursus dolor.
+              Ornare morbi lobortis tristique diam. Adipiscing sed at ut sit et,
+              consequat. Ornare diam mattis eu nunc ornare erat. Vehicula
+              ultricies orci, nunc vel. Vestibulum lacus, cursus tellus
+              consectetur nisl lorem.
+            </p>
+            <input className={styles.expand_btn} type="checkbox" />
+            <div className={styles.buttonDiv}>
+              <button className={styles.button}>Register Now</button>
+            </div>
           </div>
         </div>
       </div>

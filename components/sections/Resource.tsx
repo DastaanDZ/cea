d698294c02269e1794c0aa7405b4c35e61e83338
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2024-02-25 19:23:10 +0530

    fixed resources route

--- components/sections/Resource.tsx
+++ components/sections/Resource.tsx
@@ -13,7 +13,7 @@ export const Resource = () => {
           <ResourceCard title="CORE" />
           <ResourceCard title="CODING" />
         </div>
-        <DownArrowButton text="Load More" redirectLink="/resource" />
+        <DownArrowButton text="Load More" redirectLink="/resources" />
       </div>
     </>
   );
